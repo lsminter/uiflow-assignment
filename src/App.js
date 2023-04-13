def7c3891378b29ec64b32c9d58a45e037e95fad
Lucas Minter <lsminteris@gmail.com>
2023-04-12 18:11:12 -0700

    commented out code that isnt currently working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
 
-function App() {
+/**
+ * Goals:
+ * 1. Fetch a list of products from https://fakestoreapi.com/products
+ * 2. Display the image, title, and price for each product in a grid or list
+ * 3. Bonus: clicking on a product shows the other product attributes
+ * Example (in Uiflow): https://www.uiflow.com/apps/brian_s_sandbox_app/studio/main
+ 
+ The above link doesn't work
+ */
+ export default function App () {
+  const [items, setItems] = useState([])
+  const [showMore, setShowMore] = useState('')
+  
+  useEffect( () => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
+  // const handleButton = i => {
+  //   setShowMore(prevState => {})
+  // }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {items.map((x, i) => (
+        <div>
+          <ul key={i}>
+            {!showMore 
+              ? x.title 
+              : 
+              <div>
+                <div>{x.title}</div>
+                <div>{x.description}</div>
+              </div>
+            }
+            <button onClick={() => setShowMore(!showMore)}>{!showMore ? "Show More" : "Show Less"}</button>
+            {/* One button updates the state for all of the buttons instead of just the one.*/}
+            {/* <button onClick={() => handleButton(i)}>{!showMore ? "Show More" : "Show Less"}</button> */}
+          </ul>
+          <ul>Price: {x.price}</ul>
+          <img src={x.image} alt="text" class="img"/>
+        </div>
+      ))}
+    </ul>
   );
-}
-
-export default App;
+};
